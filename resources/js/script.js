f88a0260925225873dd4317957f4d9d36c551fc2
Edmond Thompson <edmond.j.thompson@gmail.com>
2017-04-03 12:13:13 -0400

    jQuery for scrolling navigation added

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+    /* STICKY NAVIGATION */
+
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -8,10 +11,35 @@ $(document).ready(function () {
     }, {
         offset: '60px'
     });
-});
 
-/*var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
-}, {
-  offset: '25%'
-})*/
+    /* SCROLL ON BUTTONS */
+
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+
+    /* NAVIGATION SCROLL */
+
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+});
